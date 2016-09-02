684d1c1e70e9c9c265c99af36626cdcea6093a19
Guy John <guy@rumblesan.com>
2016-09-02 22:19:52 +0100

    Add instructions module

---
+++ src/instructions.js
@@ -0,0 +1,14 @@
+
+export default {
+  0: {symbol: '^'},
+  1: {symbol: '>'},
+  2: {symbol: 'v'},
+  3: {symbol: '<'},
+  4: {symbol: '_'},
+  5: {symbol: '|'},
+  6: {symbol: '@'},
+  7: {symbol: '+'},
+  8: {symbol: '-'},
+  9: {symbol: '*'},
+  10: {symbol: '/'}
+};
