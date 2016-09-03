3daef32f2de51ad40591cb214dc9073a9db62a8a
Guy John <guy@rumblesan.com>
2016-09-03 15:51:30 +0100

    Make pointerGfx easier to translate

--- src/pointerGfx.js
+++ src/pointerGfx.js
@@ -1,10 +1,9 @@
 
 export const create = (two, grid, style) => {
-  const pointerGfx = two.makeRectangle(
+  const pointerGfx = two.makeRectangle(0, 0, grid.cellSize, grid.cellSize);
+  pointerGfx.translation.set(
     0.5 * grid.cellSize,
-    0.5 * grid.cellSize,
-    grid.cellSize,
-    grid.cellSize
+    0.5 * grid.cellSize
   );
   pointerGfx.noFill();
   pointerGfx.stroke = style.stroke;
