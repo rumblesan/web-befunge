607d8e361f4b21dbdd7c6384ee4f9b264bc64487
Guy John <guy@rumblesan.com>
2016-09-03 15:16:45 +0100

    Make cell translations less weird

--- src/cell.js
+++ src/cell.js
@@ -2,22 +2,21 @@
 
 export const create = (two, grid, xPos, yPos, instruction, cellStyle) => {
   var gfx = two.makeGroup();
-  const text = new Two.Text(
-    instruction.symbol,
-    (xPos + 0.5) * grid.cellSize,
-    (yPos + 0.5) * grid.cellSize
-  );
+  const text = new Two.Text(instruction.symbol, 0, 0);
   text.size = cellStyle.textSize;
-  const cellGfx = two.makeRectangle(
-    (xPos + 0.5) * grid.cellSize,
-    (yPos + 0.5) * grid.cellSize,
+  const cellGfx = two.makeRectangle(0, 0,
     grid.cellSize,
     grid.cellSize
   );
   cellGfx.fill = cellStyle.fill;
   cellGfx.stroke = cellStyle.stroke;
   cellGfx.linewidth = cellStyle.linewidth;
+
   gfx.add(cellGfx, text);
+  gfx.translation.set(
+    (xPos + 0.5) * grid.cellSize,
+    (yPos + 0.5) * grid.cellSize
+  );
   return {
     instruction: instruction,
     gfx: gfx
