467f90f32e688202edfaff8711f6584304b4e656
Guy John <guy@rumblesan.com>
2016-09-08 12:58:00 +0100

    Can create "modified" cells

--- src/ui/cellGfx.js
+++ src/ui/cellGfx.js
@@ -10,7 +10,7 @@ export const create = (two, style) => {
 
 };
 
-export const newCellGfx = (cellGfx, instruction, coords) => {
+export const newCellGfx = (cellGfx, instruction, coords, modified) => {
   const {two, style} = cellGfx;
   const gfx = two.makeGroup();
   const text = new Two.Text(instruction.symbol, 0, 0);
@@ -19,8 +19,13 @@ export const newCellGfx = (cellGfx, instruction, coords) => {
     style.cellSize,
     style.cellSize
   );
-  cellRect.fill = style.fill;
-  cellRect.stroke = style.stroke;
+  if (modified) {
+    cellRect.fill = style.modified.fill;
+    cellRect.stroke = style.modified.stroke;
+  } else {
+    cellRect.fill = style.normal.fill;
+    cellRect.stroke = style.normal.stroke;
+  }
   cellRect.linewidth = style.linewidth;
 
   gfx.add(cellRect, text);
