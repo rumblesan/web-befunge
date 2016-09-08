f838b76da0071e879199ef74e1ebdd033b524500
Guy John <guy@rumblesan.com>
2016-09-08 09:40:36 +0100

    Make grid get coords give cell central position

--- src/befunge/grid.js
+++ src/befunge/grid.js
@@ -10,12 +10,19 @@ export const create = (config) => {
   return grid;
 };
 
+/**
+ x, y:       Coordinates of grid cell
+ xPos, yPos: Position of original click
+ cX, cY:     Central coordinates of cell
+ */
 export const getCoordinates = (grid, scene, xPos, yPos) => {
   const x = xPos - scene.translation.x;
   const y = yPos - scene.translation.y;
-  var nearestX = Math.floor(x / grid.cellSize);
-  var nearestY = Math.floor(y / grid.cellSize);
+  const nearestX = Math.floor(x / grid.cellSize);
+  const nearestY = Math.floor(y / grid.cellSize);
+  const cX = (nearestX + 0.5) * grid.cellSize;
+  const cY = (nearestY + 0.5) * grid.cellSize;
   return {
-    x: nearestX, y: nearestY, xPos: xPos, yPos: yPos, xGrid: x, yGrid: y
+    x: nearestX, y: nearestY, xPos: xPos, yPos: yPos, cX: cX, cY: cY
   };
 };
